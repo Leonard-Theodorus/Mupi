64ba67df6d94a1352f4a729fceae6a7dca0a9273
Leonard Theodorus <aoesotfs@gmail.com>
2023-11-06 19:06:54 +0700

    Initial Commit

---
+++ Mupi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mupi
+//
+//  Created by 🐈‍⬛🐸 on 06/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
